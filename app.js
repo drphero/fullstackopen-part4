b131d4be0562a319d48cc597940a52c83488cdfa
Brent <bbenofsky@gmail.com>
2020-04-15 02:12:40 +0800

    finish exercise 4.19

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 const cors = require('cors');
 const blogsRouter = require('./controllers/blogs');
 const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 const mongoose = require('mongoose');
 const logger = require('./utils/logger');
 
@@ -27,11 +28,14 @@ app.use(express.json());
 
 app.use('/api/blogs', blogsRouter);
 app.use('/api/users', usersRouter);
+app.use('/api/login', loginRouter);
 
 const errorHandler = (error, request, response, next) => {
   logger.error(error.message);
   if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message });
+  } else if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({ error: 'invalid token' });
   }
   next(error);
 };
